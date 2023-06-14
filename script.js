bf70fb4e88a41f2b5ee3492552f40dfe00b59857
lucasaymard <aymard_l@etna-alternance.net>
2023-06-14 18:21:02 +0200

    push nouvelle idée

--- script.js
+++
@@ -1,9 +0,0 @@
-function IsMale(bool) {
-    const image = document.querySelector('.image img');
-    console.log(bool)
-    if (bool) {
-        image.src = '../Image/iop.png';
-    } else {
-        image.src = '../Image/iop_femmele.jpeg';
-    }
-}
