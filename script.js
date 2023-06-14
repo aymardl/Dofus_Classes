89115baf549cfc29eba16af68c61e2d5dc8807c1
lucasaymard <aymard_l@etna-alternance.net>
2023-06-14 21:56:11 +0200

    push page finit sans bouton

---
+++ script.js
@@ -0,0 +1,10 @@
+
+ScrollReveal().reveal('.textbox, .personage-image, .navbar',  {
+    distance: '30px', // Distance de décalage lors de la révélation
+    origin: 'bottom', // Point d'origine de la révélation
+    duration: 1000, // Durée de l'animation en millisecondes
+    delay: 200, // Délai avant le début de l'animation en millisecondes
+    easing: 'cubic-bezier(0.5, 0, 0, 1)', // Courbe d'animation
+    interval: 200 // Délai entre chaque élément révélé
+  });
+  
